dae5e06ef37e1ba6571047bf94f8456541caad7e
Geoff <geoff.register@gmail.com>
2018-10-24 23:25:16 -0400

    contact me overlay added

---
+++ main.js
@@ -0,0 +1,8 @@
+function openContact() {
+    document.getElementById("myContact").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeContact() {
+    document.getElementById("myContact").style.width = "0%";
+}
