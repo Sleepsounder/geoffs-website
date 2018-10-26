07a51e5ff9de5da18742875489f002ae88c57049
Geoff <geoff.register@gmail.com>
2018-10-26 12:58:40 -0400

    stick figure hover over functioning

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ function openContact() {
 function closeContact() {
     document.getElementById("myContact").style.width = "0%";
 }
+
+
+function hover(element) {
+    element.setAttribute('src', './images/my-stick-photo.png');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', './images/stick_me.png');
+  }
