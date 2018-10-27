16fdb01d1c06e4487f15b4dd2045bf1144d3b515
Geoff <geoff.register@gmail.com>
2018-10-27 10:35:34 -0400

    proportion stuff

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-function openContact() {
-    document.getElementById("myContact").style.width = "100%";
+function openAbout() {
+    document.getElementById("aboutMe").style.width = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
-function closeContact() {
-    document.getElementById("myContact").style.width = "0%";
+function closeAbout() {
+    document.getElementById("aboutMe").style.width = "0%";
 }
 
 
